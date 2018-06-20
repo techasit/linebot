7be64a057bfb8e80bf60a3de286df89668763cc1
techasit <37579015+techasit@users.noreply.github.com>
2018-06-20 08:26:10 +0700

    Update ิbot.php

--- ิbot.php
+++ ิbot.php
@@ -1,7 +1,7 @@
 <?php // callback.php
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '72s65lwCPZBr6eASdnEDC/OCDgrpN0w8Mp7dMGtmm1wr+iURUDY90opUxkFJ/dz3kB7EhP7RgtAFy6dLOxoA6RXPUH43Gh6e8l1JsQKTFxvWwVbybeWL7qP4AEb1uvoyIP5DjeNyWRnr6GFN7qXaVgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
